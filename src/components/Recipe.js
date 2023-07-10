9a0761e2d7f8a31c6ba177a0483933c8809b5409
bintadam <zakiyaadan12@gmail.com>
2023-07-10 17:14:50 +0200

    add useState

---
+++ src/components/Recipe.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Recipe(){
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState("");
+    const [list, setList] = useState("alcohol-free");
+    return (
+        <div></div>
+    )
+}
+
+export default Recipe;
