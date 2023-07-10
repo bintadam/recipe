664c7f2a909d0f59b5feea5d847a5e82723a3b27
bintadam <zakiyaadan12@gmail.com>
2023-07-10 17:25:20 +0200

    add fetchdata

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,19 @@
+import axios from "axios";
 import { useState } from "react";
 
 function Recipe(){
     const [data, setData] = useState([]);
     const [query, setQuery] = useState("");
     const [list, setList] = useState("alcohol-free");
+
+    const appId = "c2de284e"
+    const apiKey = "1aab020382a4c2bdd14e08d165139f40";
+
+    const fetchData = async() => {
+        const response = await axios.get(`https://api.edamam.com/search?q=${searchString}app_id=${appId}&app_key=${apiKey}&health=${list}`)
+        setData(response.data.hits)
+        console.log(response.data)
+    }
     return (
         <div></div>
     )
