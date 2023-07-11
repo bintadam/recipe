460f4935a4ca339214c3316549a38fb163d2658b
bintadam <zakiyaadan12@gmail.com>
2023-07-11 22:50:22 +0200

    add route component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Link from "./components/Link";
+import Route from "./components/Route";
+import Home from "./components/Home";
+import Recipe from "./components/Recipe";
 
 function App() {
   return (
     <div>
-
+      <Route path="/">
+        <Home/>
+      </Route>
+      <Route path="/recipe">
+        <Recipe/>
+      </Route>
     </div>
   );
 }
