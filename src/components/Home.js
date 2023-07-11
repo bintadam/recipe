f081f51cfcb36dfb4af907b20f7f7813484426e8
bintadam <zakiyaadan12@gmail.com>
2023-07-11 16:08:46 +0200

    add style

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import heroImage from "../images/images.jpeg";
 function Home() {
   return (
     <div className="relative">
+            <div className="absolute left-25">
+                <h1 className=" text-3xl p-6 text-white font-thin ">" Search for simple & <br></br>healthy delicious <br></br>recipes fo enjoy "</h1>
+                <p className="bg-white px-0 py-4">Get the recipe now </p>
+        </div>
         <img  className="bg-cover bg-no-repeat" src={heroImage} alt="ingredient"/>
-        <h1 className="flex absolute top-20  text-3xl p-6 font-thin text-white">" Search for simple & healthy delicious recipes fo enjoy "</h1>
     </div>
   );
 }
